6e72e011497658a32ded48c3aec5bdcf2ed3593d
vinsentli <vinsentli@tencent.com>
2024-06-13 17:14:18 +0800

    NDK21对于std::filesystem支持不完整，先注释掉相关调用。

--- shell/shared/fileLoader/android/FileLoaderAndroid.cpp
+++ shell/shared/fileLoader/android/FileLoaderAndroid.cpp
@@ -21,43 +21,45 @@ FileLoader::FileData FileLoaderAndroid::loadBinaryData(const std::string& fileNa
     return {};
   }
 
-  // @fb-only
-    auto path = std::filesystem::path("/data/local/tmp"); // Path for boltbench assets
-    return loadBinaryDataInternal(path / fileName);
-    // @fb-only
-  // @fb-only
-
-  if (assetManager_ == nullptr) {
-    IGL_LOG_INFO("Asset manager not set!\n");
-    // Fallback to default behavior (i.e., loading w/ C++ functions) when asset manager is not set
-    // as this is the case for some unit tests.
-    return loadBinaryDataInternal(fileName);
-  }
-
-  // Load file
-  AAsset* asset = AAssetManager_open(assetManager_, fileName.c_str(), AASSET_MODE_BUFFER);
-  if (IGL_UNEXPECTED(asset == nullptr)) {
-    IGL_LOG_ERROR("Error in loadBinaryData(): failed to open file %s\n", fileName.c_str());
     return {};
-  }
 
-  off64_t length = AAsset_getLength64(asset);
-  if (IGL_UNEXPECTED(length > std::numeric_limits<uint32_t>::max())) {
-    AAsset_close(asset);
-    return {};
-  }
-
-  auto data = std::make_unique<uint8_t[]>(length);
-  auto readSize = AAsset_read(asset, data.get(), length);
-  if (IGL_UNEXPECTED(readSize != length)) {
-    IGL_LOG_ERROR("Error in loadBinaryData(): read size mismatch (%ld != %zu) in %s\n",
-                  readSize,
-                  length,
-                  fileName.c_str());
-  }
-  AAsset_close(asset);
-
-  return {std::move(data), static_cast<uint32_t>(length)};
+//  // @fb-only
+//    auto path = std::filesystem::path("/data/local/tmp"); // Path for boltbench assets
+//    return loadBinaryDataInternal(path / fileName);
+//    // @fb-only
+//  // @fb-only
+//
+//  if (assetManager_ == nullptr) {
+//    IGL_LOG_INFO("Asset manager not set!\n");
+//    // Fallback to default behavior (i.e., loading w/ C++ functions) when asset manager is not set
+//    // as this is the case for some unit tests.
+//    return loadBinaryDataInternal(fileName);
+//  }
+//
+//  // Load file
+//  AAsset* asset = AAssetManager_open(assetManager_, fileName.c_str(), AASSET_MODE_BUFFER);
+//  if (IGL_UNEXPECTED(asset == nullptr)) {
+//    IGL_LOG_ERROR("Error in loadBinaryData(): failed to open file %s\n", fileName.c_str());
+//    return {};
+//  }
+//
+//  off64_t length = AAsset_getLength64(asset);
+//  if (IGL_UNEXPECTED(length > std::numeric_limits<uint32_t>::max())) {
+//    AAsset_close(asset);
+//    return {};
+//  }
+//
+//  auto data = std::make_unique<uint8_t[]>(length);
+//  auto readSize = AAsset_read(asset, data.get(), length);
+//  if (IGL_UNEXPECTED(readSize != length)) {
+//    IGL_LOG_ERROR("Error in loadBinaryData(): read size mismatch (%ld != %zu) in %s\n",
+//                  readSize,
+//                  length,
+//                  fileName.c_str());
+//  }
+//  AAsset_close(asset);
+//
+//  return {std::move(data), static_cast<uint32_t>(length)};
 }
 
 bool FileLoaderAndroid::fileExists(const std::string& fileName) const {
@@ -76,47 +78,48 @@ bool FileLoaderAndroid::fileExists(const std::string& fileName) const {
     }
     return false;
   } else {
-    return std::filesystem::exists(fileName);
+      return false;
+//    return std::filesystem::exists(fileName);
   }
 }
 
 std::string FileLoaderAndroid::basePath() const {
   std::string basePath;
-  if (assetManager_) {
-    AAssetDir* assetDir = AAssetManager_openDir(assetManager_, "");
-    if (assetDir != nullptr) {
-      const char* fileName = AAssetDir_getNextFileName(assetDir);
-      std::filesystem::path filePath(fileName);
-      basePath = filePath.root_path();
-      AAssetDir_close(assetDir);
-    }
-  } else {
-    basePath = std::filesystem::current_path().string();
-  }
+//  if (assetManager_) {
+//    AAssetDir* assetDir = AAssetManager_openDir(assetManager_, "");
+//    if (assetDir != nullptr) {
+//      const char* fileName = AAssetDir_getNextFileName(assetDir);
+//      std::filesystem::path filePath(fileName);
+//      basePath = filePath.root_path();
+//      AAssetDir_close(assetDir);
+//    }
+//  } else {
+//    basePath = std::filesystem::current_path().string();
+//  }
   return basePath;
 }
 
 std::string FileLoaderAndroid::fullPath(const std::string& fileName) const {
-  if (assetManager_) {
+//  if (assetManager_) {
     return fileName;
-  } else {
-    if (std::filesystem::exists(fileName)) {
-      return fileName;
-    }
-
-    auto fullPath = std::filesystem::path("/data/local/tmp") / fileName;
-    if (std::filesystem::exists(fullPath)) {
-      return fullPath.string();
-    }
-
-    fullPath = std::filesystem::temp_directory_path() / fileName;
-    if (std::filesystem::exists(fullPath)) {
-      return fullPath.string();
-    }
-
-    IGL_ASSERT_NOT_REACHED();
-    return "";
-  }
+//  } else {
+//    if (std::filesystem::exists(fileName)) {
+//      return fileName;
+//    }
+//
+//    auto fullPath = std::filesystem::path("/data/local/tmp") / fileName;
+//    if (std::filesystem::exists(fullPath)) {
+//      return fullPath.string();
+//    }
+//
+//    fullPath = std::filesystem::temp_directory_path() / fileName;
+//    if (std::filesystem::exists(fullPath)) {
+//      return fullPath.string();
+//    }
+//
+//    IGL_ASSERT_NOT_REACHED();
+//    return "";
+//  }
 }
 
 } // namespace igl::shell
