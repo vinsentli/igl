bf6687b2c9046f867181ecd7d768897710c7e753
vinsentli <vinsentli@tencent.com>
2024-06-14 10:11:47 +0800

    vulkan和main分支保持一致。

--- src/igl/vulkan/VulkanShaderModule.cpp
+++ src/igl/vulkan/VulkanShaderModule.cpp
@@ -7,56 +7,6 @@
 
 #include <igl/vulkan/VulkanShaderModule.h>
 
-<<<<<<< HEAD
-#if IGL_USE_GLSLANG
-#include <glslang/Include/glslang_c_interface.h>
-#endif
-
-#include <igl/vulkan/Common.h>
-
-namespace {
-
-// Logs GLSL shaders with line numbers annotation
-void logShaderSource(const char* text) {
-#if IGL_DEBUG
-  uint32_t line = 1;
-
-  // IGLLog on Android also writes a new line,
-  // so to make things easier to read separate out the Android logging
-#if IGL_PLATFORM_ANDROID
-  std::string outputLine = "";
-  while (text && *text) {
-    if (*text == '\n') {
-      // Write out the line along with the line number, and reset the line
-      IGL_LOG_INFO("(%3u) %s", line++, outputLine.c_str());
-      outputLine = "";
-    } else if (*text == '\r') {
-      // skip it
-    } else {
-      outputLine += *text;
-    }
-    text++;
-  }
-  IGL_LOG_INFO("");
-#else
-  IGL_LOG_INFO("\n(%3u) ", line);
-  while (text && *text) {
-    if (*text == '\n') {
-      IGL_LOG_INFO("\n(%3u) ", ++line);
-    } else if (*text == '\r') {
-      // skip it to support Windows/UNIX EOLs
-    } else {
-      IGL_LOG_INFO("%c", *text);
-    }
-    text++;
-  }
-  IGL_LOG_INFO("\n");
-#endif
-#endif
-}
-
-} // namespace
-
 namespace igl {
 namespace vulkan {
 
--- src/igl/vulkan/VulkanShaderModule.h
+++ src/igl/vulkan/VulkanShaderModule.h
@@ -17,6 +17,7 @@
 
 namespace igl {
 namespace vulkan {
+  
 /**
  * @brief RAII wrapper for a Vulkan shader module.
  */
