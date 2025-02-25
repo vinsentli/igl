09f0659ed4699d58633ee7b370482f026168c116
vinsentli <vinsentli@tencent.com>
2025-02-25 19:15:46 +0800

    --bug=igl | vulkan | Fix memory leak for pipelineLayout if there are many dynamicStates in RenderPipelineState.

--- src/igl/vulkan/RenderPipelineState.cpp
+++ src/igl/vulkan/RenderPipelineState.cpp
@@ -398,21 +398,23 @@ VkPipeline RenderPipelineState::getVkPipeline(
       ctx.getBindlessVkDescriptorSetLayout(),
   };
 
-  const VkPipelineLayoutCreateInfo ci =
-      ivkGetPipelineLayoutCreateInfo(static_cast<uint32_t>(ctx.config_.enableDescriptorIndexing
-                                                               ? IGL_ARRAY_NUM_ELEMENTS(DSLs)
-                                                               : IGL_ARRAY_NUM_ELEMENTS(DSLs) - 1u),
-                                     DSLs,
-                                     info_.hasPushConstants ? &pushConstantRange_ : nullptr);
+  if (!pipelineLayout_) {
+    const VkPipelineLayoutCreateInfo ci = ivkGetPipelineLayoutCreateInfo(
+        static_cast<uint32_t>(ctx.config_.enableDescriptorIndexing
+                                  ? IGL_ARRAY_NUM_ELEMENTS(DSLs)
+                                  : IGL_ARRAY_NUM_ELEMENTS(DSLs) - 1u),
+        DSLs,
+        info_.hasPushConstants ? &pushConstantRange_ : nullptr);
 
-  VkDevice device = ctx.device_->getVkDevice();
-  VK_ASSERT(ctx.vf_.vkCreatePipelineLayout(device, &ci, nullptr, &pipelineLayout_));
-  VK_ASSERT(
-      ivkSetDebugObjectName(&ctx.vf_,
-                            device,
-                            VK_OBJECT_TYPE_PIPELINE_LAYOUT,
-                            (uint64_t)pipelineLayout_,
-                            IGL_FORMAT("Pipeline Layout: {}", desc_.debugName.c_str()).c_str()));
+    VkDevice device = ctx.device_->getVkDevice();
+    VK_ASSERT(ctx.vf_.vkCreatePipelineLayout(device, &ci, nullptr, &pipelineLayout_));
+    VK_ASSERT(
+        ivkSetDebugObjectName(&ctx.vf_,
+                              device,
+                              VK_OBJECT_TYPE_PIPELINE_LAYOUT,
+                              (uint64_t)pipelineLayout_,
+                              IGL_FORMAT("Pipeline Layout: {}", desc_.debugName.c_str()).c_str()));
+  }
 
   const auto& deviceFeatures = ctx.features();
   const VkBool32 dualSrcBlendSupported =
