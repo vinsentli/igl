d2c868c943afb1f9d4137ee6be7a1ea57411bf28
vinsentli <vinsentli@tencent.com>
2026-05-03 14:22:04 -0700

    igl | vulkan | If enable VK_ext_descriptor_buffer , add VK_PIPELINE_CREATE_DESCRIPTOR_BUFFER_BIT_EXT flag for compute pipeline and graphic pipeline. (#362)
    Summary: Pull Request resolved: https://github.com/facebook/igl/pull/362
    
    Reviewed By: maliarov
    
    Differential Revision: D103254370
    
    Pulled By: corporateshark
    
    fbshipit-source-id: 5da0db6892f1ec656f989226fbf677b1819eab87

--- src/igl/vulkan/ComputePipelineState.cpp
+++ src/igl/vulkan/ComputePipelineState.cpp
@@ -96,6 +96,9 @@ VkPipeline ComputePipelineState::getVkPipeline() const {
       .pPushConstantRanges = info.hasPushConstants ? &pushConstantRange : nullptr,
   };
 
+  const VkPipelineCreateFlags flags = ctx.features().has_VK_EXT_descriptor_buffer
+                                          ? VK_PIPELINE_CREATE_DESCRIPTOR_BUFFER_BIT_EXT
+                                          : VkPipelineCreateFlags{};
   const VkDevice device = ctx.getVkDevice();
   VK_ASSERT(ctx.vf_.vkCreatePipelineLayout(device, &ci, nullptr, &pipelineLayout));
   VK_ASSERT(
@@ -114,8 +117,13 @@ VkPipeline ComputePipelineState::getVkPipeline() const {
           .module = igl::vulkan::ShaderModule::getVkShaderModule(shaderModule),
           .pName = shaderModule->info().entryPoint.c_str(),
       })
-      .build(
-          ctx.vf_, device, ctx.pipelineCache_, pipelineLayout, &pipeline_, desc_.debugName.c_str());
+      .build(ctx.vf_,
+             device,
+             flags,
+             ctx.pipelineCache_,
+             pipelineLayout,
+             &pipeline_,
+             desc_.debugName.c_str());
 
   return pipeline_;
 }
