2083cee2637b1a35d91cf0fac46513c4dd92f86d
Alexey Medvedev <amedvedev@meta.com>
2026-05-07 00:31:25 -0700

    Type annotations: style.py
    Reviewed By: rameshviswanathan
    
    Differential Revision: D104161901
    
    fbshipit-source-id: f777fe8d76aae4a1e368f42c4d5810aacc9c4ece

--- docs/source/_extensions/style.py
+++ docs/source/_extensions/style.py
@@ -1,10 +1,19 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from __future__ import annotations
+
+from typing import Any
+
 from pygments.filters import VisibleWhitespaceFilter
 from pygments.lexers.compiled import CppLexer, RustLexer
 from pygments.lexers.make import CMakeLexer
 from sphinx.highlighting import lexers
 
 
-def setup(app):
+def setup(app: Any) -> None:
     """Replace tabs with 4 spaces"""
     lexers["C++"] = CppLexer()
     lexers["rust"] = RustLexer()
